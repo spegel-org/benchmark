791ea74e1da9c2b315a6b3085e6bb765103e8502
Philip Laine <philip.laine@gmail.com>
2025-05-31 16:55:41 +0200

    Use box charts to visualize performance

--- internal/analyze/analyze.go
+++ internal/analyze/analyze.go
@@ -3,14 +3,13 @@ package analyze
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"image/color"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
 
-	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
@@ -20,84 +19,128 @@ import (
 	"github.com/spegel-org/benchmark/internal/measure"
 )
 
-func Analyze(ctx context.Context, path string) error {
-	b, err := os.ReadFile(path)
+func Analyze(ctx context.Context, baselineDir, variantDir, outputDir string) error {
+	entries, err := os.ReadDir(baselineDir)
 	if err != nil {
 		return err
 	}
-	result := measure.Result{}
-	err = json.Unmarshal(b, &result)
-	if err != nil {
-		return err
-	}
-	ext := filepath.Ext(path)
-	outPath := strings.TrimSuffix(path, ext)
-	outPath = fmt.Sprintf("%s.png", outPath)
-	err = createPlot(result, outPath)
-	if err != nil {
-		return err
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		if !strings.HasSuffix(entry.Name(), ".json") {
+			continue
+		}
+		b, err := os.ReadFile(filepath.Join(baselineDir, entry.Name()))
+		if err != nil {
+			return err
+		}
+		baseline := measure.Result{}
+		err = json.Unmarshal(b, &baseline)
+		if err != nil {
+			return err
+		}
+		b, err = os.ReadFile(filepath.Join(variantDir, entry.Name()))
+		if err != nil {
+			return err
+		}
+		variant := measure.Result{}
+		err = json.Unmarshal(b, &variant)
+		if err != nil {
+			return err
+		}
+		ext := filepath.Ext(entry.Name())
+		outputPath := strings.TrimSuffix(entry.Name(), ext)
+		outputPath = fmt.Sprintf("%s.png", outputPath)
+		outputPath = filepath.Join(outputDir, outputPath)
+		err = createBoxPlot(baseline, variant, outputPath)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func createPlot(result measure.Result, path string) error {
+func createBoxPlot(baseline, variant measure.Result, outputPath string) error {
+	if len(baseline.Benchmarks) != len(variant.Benchmarks) {
+		return errors.New("results cant have different benchmark counts")
+	}
+
 	plots := []*plot.Plot{}
-	for _, bench := range result.Benchmarks {
-		p := plot.New()
-		p.Title.Text = bench.Image
-		p.Title.Padding = vg.Points(10)
-		p.Y.Min = 0
-		p.Y.Label.Text = "Pod Number"
-		p.X.Label.Padding = 3
-		slices.SortFunc(bench.Measurements, func(a, b measure.Measurement) int {
-			if a.Start.Equal(b.Start) {
-				return a.Stop.Compare(b.Stop)
-			}
-			return a.Start.Compare(b.Start)
-		})
-		zeroTime := bench.Measurements[0].Start
-
-		sum := int64(0)
-		durations := []float64{}
-		for i, result := range bench.Measurements {
-			sum += result.Duration.Milliseconds()
-			durations = append(durations, float64(result.Duration.Milliseconds()))
-
-			start := result.Start.Sub(zeroTime)
-			stop := start + result.Duration
-			b, err := plotter.NewBoxPlot(4, float64(len(bench.Measurements)-i-1), plotter.Values{float64(start.Milliseconds()), float64(stop.Milliseconds())})
-			if err != nil {
-				return err
-			}
-			b.Horizontal = true
-			b.FillColor = color.Black
-			p.Add(b)
+	w := vg.Points(30)
+	for i := range len(baseline.Benchmarks) {
+		if len(baseline.Benchmarks[i].Measurements) != len(variant.Benchmarks[i].Measurements) {
+			return errors.New("benchmarks cant have different measurement counts")
 		}
-		slices.Sort(durations)
-		mean := stat.Mean(durations, nil)
-		p90 := stat.Quantile(0.90, stat.Empirical, durations, nil)
-		p95 := stat.Quantile(0.95, stat.Empirical, durations, nil)
-		p.X.Label.Text = fmt.Sprintf("Time [ms}\n\nMean: %.0f ms | P90: %.0f ms | P95: %.0f ms | Total: %d ms", mean, p90, p95, sum)
+		if baseline.Benchmarks[i].Image != variant.Benchmarks[i].Image {
+			return errors.New("benchmark images are not the same")
+		}
+
+		p := plot.New()
+		p.Title.Text = baseline.Benchmarks[i].Image
+		p.Title.Padding = vg.Points(10)
+		p.Y.Label.Text = "Pull Time (seconds)"
+
+		durations := plotter.Values{}
+		for _, meas := range baseline.Benchmarks[i].Measurements {
+			durations = append(durations, float64(meas.Duration.Seconds()))
+		}
+		boxPlot, err := plotter.NewBoxPlot(w, float64(0), durations)
+		if err != nil {
+			return err
+		}
+		boxPlot.FillColor = color.RGBA{R: 30, G: 144, B: 255, A: 255}
+		p.Add(boxPlot)
+
+		durations = plotter.Values{}
+		for _, meas := range variant.Benchmarks[i].Measurements {
+			durations = append(durations, float64(meas.Duration.Seconds()))
+		}
+		boxPlot, err = plotter.NewBoxPlot(w, float64(1), durations)
+		if err != nil {
+			return err
+		}
+		boxPlot.FillColor = color.RGBA{R: 220, G: 20, B: 60, A: 255}
+		p.Add(boxPlot)
+
+		p.NominalX("Baseline", "Spegel")
 		plots = append(plots, p)
 	}
 
-	img := vgimg.New(vg.Points(700), vg.Points(350))
+	width := vg.Points(500)
+	height := vg.Points(500)
+	img := vgimg.New(width, height)
 	dc := draw.New(img)
+
+	// Draw shared title.
+	titleStyle := draw.TextStyle{
+		Font:    plot.DefaultFont,
+		Color:   color.Black,
+		XAlign:  draw.XCenter,
+		YAlign:  draw.YTop,
+		Handler: plot.DefaultTextHandler,
+	}
+	titleStyle.Font.Size = vg.Points(12)
+	titlePoint := vg.Point{X: width / 2, Y: height - vg.Millimeter}
+	dc.FillText(titleStyle, titlePoint, "Image Pull Duration")
+
+	// Draw plots.
 	t := draw.Tiles{
 		Rows:      1,
 		Cols:      len(plots),
-		PadX:      vg.Millimeter,
+		PadX:      vg.Points(40),
 		PadY:      vg.Millimeter,
-		PadTop:    vg.Points(10),
-		PadBottom: vg.Points(10),
-		PadLeft:   vg.Points(10),
-		PadRight:  vg.Points(10),
+		PadTop:    vg.Points(35),
+		PadBottom: vg.Points(5),
+		PadLeft:   vg.Points(20),
+		PadRight:  vg.Points(20),
 	}
 	canv := plot.Align([][]*plot.Plot{plots}, t, dc)
 	for i, plot := range plots {
 		plot.Draw(canv[0][i])
 	}
-	file, err := os.Create(path)
+
+	file, err := os.Create(outputPath)
 	if err != nil {
 		return err
 	}
